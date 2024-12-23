fa0edcb04411c79717ec45853fc99814255c0f96
Sarah Wang <millimillenary@gmail.com>
2024-12-24 09:30:57 +1100

    Add button to toggle mute

--- src/remote_control_handler.py
+++ src/remote_control_handler.py
@@ -3,6 +3,7 @@ Remote control HTTP request handler.
 """
 
 import io
+import subprocess
 from http.server import BaseHTTPRequestHandler
 
 
@@ -16,3 +17,17 @@ class RemoteControlHandler(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(io.FileIO("index.html").read())
+
+    # pylint: disable-next=C0103
+    def do_POST(self) -> None:
+        """Handle POST requests."""
+        if self.path == "/mute":
+            self.send_response(200)  # 200: OK
+            self.end_headers()
+            subprocess.run(
+                ["/usr/bin/pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle"],
+                check=True,
+            )
+        else:
+            self.send_response(400)  # 400: Bad Request
+            self.end_headers()
